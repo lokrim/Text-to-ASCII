2df772f22e02f6d77a5de31d7c031c31670c3e01
lokrim <ahmedzyed123@gmail.com>
2025-04-17 02:51:59 +0530

    Added Slant Style

--- index.js
+++ index.js
@@ -5,10 +5,7 @@ function textToASCII(string){
         i = i.toUpperCase();
         letters.push(characters[i]);
     }
-    let TopBottomBorder = "";
-    for (let j=0; j<letters.length;j++){
-        TopBottomBorder += "---------";
-    }
+    let TopBottomBorder = "---------".repeat(letters.length);
     console.log("   " + TopBottomBorder);
     let rowNum = letters[0].length;
     for (let row=0; row<rowNum; row++){
@@ -22,4 +19,29 @@ function textToASCII(string){
     console.log("   " + TopBottomBorder);
 }
 
+function textToASCIISlant(string){
+    let letters = [];
+    for (let i of string){
+        i = i.toUpperCase();
+        letters.push(characters[i]);
+    }
+    let TopBottomBorder = "----------".repeat(letters.length);
+    console.log(" " + TopBottomBorder);
+    let rowNum = letters[0].length;
+    let slant = " ".repeat(rowNum);
+    for (let row=0; row<rowNum; row++){
+        let line = "|  " + slant;
+        slant = slant.slice(0,slant.length - 1);
+        for (let char=0; char<letters.length; char++){
+            line += letters[char][row] + "  ";
+        }
+        let trailingSpaces = " ".repeat(row);
+        line += trailingSpaces + "  |";
+        console.log(line);
+    }
+    console.log(" " + TopBottomBorder);
+}
+
 textToASCII("hello World");
+textToASCIISlant("Hello World");
+textToASCIISlant("Ahmed Zyed");
