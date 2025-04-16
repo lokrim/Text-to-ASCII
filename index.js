9513b5bd6f1288fdae58138ce7b9edea376297ae
lokrim <ahmedzyed123@gmail.com>
2025-04-17 02:10:00 +0530

    Added Top and Bottom Borders.

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 const characters = require('./characters.json');
 function textToASCII(string){
-    let temp = [];
+    let letters = [];
     for (let i of string){
         i = i.toUpperCase();
-        temp.push(characters[i]);
+        letters.push(characters[i]);
     }
-    let rowNum = temp[0].length;
+    let TopBottomBorder = "";
+    for (let j=0; j<letters.length;j++){
+        TopBottomBorder += "---------";
+    }
+    console.log("   " + TopBottomBorder);
+    let rowNum = letters[0].length;
     for (let row=0; row<rowNum; row++){
         let line = "|  ";
-        for (let char=0; char<temp.length; char++){
-            line += temp[char][row] + "  ";
+        for (let char=0; char<letters.length; char++){
+            line += letters[char][row] + "  ";
         }
         line += "  |";
         console.log(line);
     }
+    console.log("   " + TopBottomBorder);
 }
 
-textToASCII("Hello world");
+textToASCII("hello World");
